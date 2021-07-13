b659b2167a4937e16b4e4232d204f9f160249d30
Nate Watterson <nwatterson@nvidia.com>
2021-07-13 01:28:51 -0400

    iommu/arm-smmu-v3: Add in-kernel support for NVIDIA Tegra241 (Grace) CMDQ-V
    NVIDIA's Tegra241 Soc has a CMDQ-Virtualization (CMDQV) hardware, extending
    the standard ARM SMMU v3 IP to support multiple VCMDQs with virtualization
    capabilities. In terms of command queue, they are very like a standard SMMU
    CMDQ (or ECMDQs), but only support CS_NONE in the CS field of CMD_SYNC.
    
    Add a new tegra241-cmdqv driver, and insert its structure pointer into the
    existing arm_smmu_device, and then add related function calls in the SMMUv3
    driver to interact with the CMDQV driver.
    
    In the CMDQV driver, add a minimal part for the in-kernel support: reserve
    VINTF0 for in-kernel use, and assign some of the VCMDQs to the VINTF0, and
    select one VCMDQ based on the current CPU ID to execute supported commands.
    
    The other part of the CMDQV driver will be user-space support that gives a
    hypervisor running on the host OS to talk to the driver for virtualization
    use cases. This is currently being designed based on IOMMUFD, and patches
    will be sent for review once SMMU nesting patches are getting merged.
    
    However, it is very important for this in-kernel support to get merged and
    installed to VMs running on Grace-powered servers as soon as possible. So,
    later those servers would only need to upgrade their host kernels for the
    user-space support.
    
    As the initial version, the CMDQV driver only supports ACPI configurations.
    
    Regarding performance, the multi-queue design for in-kernel support gives
    some limited improvements: up-to-20% reduction of time with multi-threaded
    DMA unmap tests. On the other hand, the user-space support gives a bigger
    improvement: 70%-to-90% reduction of time with various unmap tests running
    in the guest OS, compared to the trapping solution of nested SMMU.
    
    Signed-off-by: Nate Watterson <nwatterson@nvidia.com>
    Co-developed-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -354,6 +354,9 @@ static int arm_smmu_cmdq_build_cmd(u64 *cmd, struct arm_smmu_cmdq_ent *ent)
 
 static struct arm_smmu_cmdq *arm_smmu_get_cmdq(struct arm_smmu_device *smmu)
 {
+	if (smmu->tegra241_cmdqv)
+		return tegra241_cmdqv_get_cmdq(smmu);
+
 	return &smmu->cmdq;
 }
 
@@ -3095,12 +3098,10 @@ static struct iommu_ops arm_smmu_ops = {
 };
 
 /* Probing and initialisation functions */
-static int arm_smmu_init_one_queue(struct arm_smmu_device *smmu,
-				   struct arm_smmu_queue *q,
-				   void __iomem *page,
-				   unsigned long prod_off,
-				   unsigned long cons_off,
-				   size_t dwords, const char *name)
+int arm_smmu_init_one_queue(struct arm_smmu_device *smmu,
+			    struct arm_smmu_queue *q, void __iomem *page,
+			    unsigned long prod_off, unsigned long cons_off,
+			    size_t dwords, const char *name)
 {
 	size_t qsz;
 
@@ -3635,6 +3636,12 @@ static int arm_smmu_device_reset(struct arm_smmu_device *smmu, bool bypass)
 		return ret;
 	}
 
+	if (smmu->tegra241_cmdqv) {
+		ret = tegra241_cmdqv_device_reset(smmu);
+		if (ret)
+			return ret;
+	}
+
 	return 0;
 }
 
@@ -3927,6 +3934,9 @@ static int arm_smmu_device_acpi_probe(struct platform_device *pdev,
 
 	if (iort_smmu->flags & ACPI_IORT_SMMU_V3_COHACC_OVERRIDE)
 		smmu->features |= ARM_SMMU_FEAT_COHERENCY;
+
+	smmu->tegra241_cmdqv =
+		tegra241_cmdqv_acpi_probe(smmu, node->identifier);
 
 	return 0;
 }
