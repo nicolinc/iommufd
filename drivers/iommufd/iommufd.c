39fdebd3246f2e8f22114b76dd408781fab83dfc
Liu Yi L <yi.l.liu@intel.com>
2021-10-26 20:28:18 +0800

    iommufd: Add iommufd_[un]bind_device()
    Under the /dev/iommu model, iommufd provides the interface for I/O page
    tables management such as dma map/unmap. However, it cannot work
    independently since the device is still owned by the device-passthrough
    frameworks (VFIO, vDPA, etc.) and vice versa. Device-passthrough frameworks
    should build a connection between its device and the iommufd to delegate
    the I/O page table management affairs to iommufd.
    
    This patch introduces iommufd_[un]bind_device() helpers for the device-
    passthrough framework to build such connection. The helper functions then
    invoke iommu core (iommu_device_init/exit_user_dma()) to establish/exit
    security context for the bound device. Each successfully bound device is
    internally tracked by an iommufd_device object. This object is returned
    to the caller for subsequent attaching operations on the device as well.
    
    The caller should pass a user-provided cookie to mark the device in the
    iommufd. Later this cookie will be used to represent the device in iommufd
    uAPI, e.g. when querying device capabilities or handling per-device I/O
    page faults. One alternative is to have iommufd allocate a device label
    and return to the user. Either way works, but cookie is slightly preferred
    per earlier discussion as it may allow the user to inject faults slightly
    faster without ID->vRID lookup.
    
    iommu_[un]bind_device() functions are only used for physical devices. Other
    variants will be introduced in the future, e.g.:
    
    -  iommu_[un]bind_device_pasid() for mdev/subdev which requires pasid granular
       DMA isolation;
    -  iommu_[un]bind_sw_mdev() for sw mdev which relies on software measures
       instead of iommu to isolate DMA;
    
    Signed-off-by: Liu Yi L <yi.l.liu@intel.com>

--- drivers/iommufd/iommufd.c
+++ drivers/iommufd/iommufd.c
@@ -16,9 +16,29 @@
 #include <linux/miscdevice.h>
 #include <linux/mutex.h>
 #include <linux/iommu.h>
+#include <linux/iommufd.h>
+#include <linux/xarray.h>
+#include <asm-generic/bug.h>
 
 /* Per iommufd */
 struct iommufd_ctx {
+	struct file *filp;
+	struct xarray device_xa; /* xarray of bound devices */
+};
+
+/*
+ * A iommufd_device object represents the binding relationship
+ * between iommufd and device. It is created per a successful
+ * binding request from device driver. The bound device must be
+ * a physical device so far. Subdevice will be supported later
+ * (with additional PASID information). An user-assigned cookie
+ * is also recorded to mark the device in the /dev/iommu uAPI.
+ */
+struct iommufd_device {
+	unsigned int devid;
+	struct iommufd_ctx *ictx;
+	struct device *dev; /* always be the physical device */
+	u64 dev_cookie;
 };
 
 static int iommufd_fops_open(struct inode *inode, struct file *filp)
@@ -30,15 +50,43 @@ static int iommufd_fops_open(struct inode *inode, struct file *filp)
 	if (!ictx)
 		return -ENOMEM;
 
+	xa_init_flags(&ictx->device_xa, XA_FLAGS_ALLOC1);
+	ictx->filp = filp;
 	filp->private_data = ictx;
 
 	return ret;
+}
+
+static const struct file_operations iommufd_fops;
+
+/**
+ * iommufd_fget - Acquires a reference to the iommufd file.
+ * @fd: [in] iommufd file descriptor.
+ *
+ * Returns a pointer to the file, otherwise NULL;
+ *
+ */
+static struct file *iommufd_fget(int fd)
+{
+	struct file *filp;
+
+	filp = fget(fd);
+	if (!filp)
+		return NULL;
+
+	if (filp->f_op != &iommufd_fops) {
+		fput(filp);
+		return NULL;
+	}
+
+	return filp;
 }
 
 static int iommufd_fops_release(struct inode *inode, struct file *filp)
 {
 	struct iommufd_ctx *ictx = filp->private_data;
 
+	WARN_ON(!xa_empty(&ictx->device_xa));
 	kfree(ictx);
 
 	return 0;
@@ -70,6 +118,82 @@ static struct miscdevice iommu_misc_dev = {
 	.nodename = "iommu",
 	.mode = 0666,
 };
+
+/**
+ * iommufd_bind_device - Bind a physical device marked by a device
+ *			 cookie to an iommu fd.
+ * @fd:		[in] iommufd file descriptor.
+ * @dev:	[in] Pointer to a physical device struct.
+ * @dev_cookie:	[in] A cookie to mark the device in /dev/iommu uAPI.
+ *
+ * A successful bind establishes a security context for the device
+ * and returns struct iommufd_device pointer. Otherwise returns
+ * error pointer.
+ *
+ */
+struct iommufd_device *iommufd_bind_device(int fd, struct device *dev,
+					   u64 dev_cookie)
+{
+	struct file *filp;
+	struct iommufd_ctx *ictx;
+	struct iommufd_device *idev;
+	int ret;
+
+	filp = iommufd_fget(fd);
+	if (!filp)
+		return ERR_PTR(-EINVAL);
+
+	ictx = filp->private_data;
+
+	idev = kzalloc(sizeof(*idev), GFP_KERNEL);
+	if (!idev) {
+		ret = -ENOMEM;
+		goto out_file_put;
+	}
+
+	/* Set the DMA owner to be userspace */
+	ret = iommu_device_set_dma_owner(dev, DMA_OWNER_USER, filp);
+	if (ret)
+		goto out_free;
+
+	idev->ictx = ictx;
+	idev->dev = dev;
+	idev->dev_cookie = dev_cookie;
+
+	ret = xa_alloc(&ictx->device_xa, &idev->devid, idev,
+		       xa_limit_32b, GFP_KERNEL);
+	if (ret)
+		goto out_release_owner;
+
+	return idev;
+
+out_release_owner:
+	iommu_device_release_dma_owner(dev, DMA_OWNER_USER);
+out_free:
+	kfree(idev);
+out_file_put:
+	fput(filp);
+	return ERR_PTR(ret);
+}
+EXPORT_SYMBOL_GPL(iommufd_bind_device);
+
+/**
+ * iommufd_unbind_device - Unbind a physical device from iommufd
+ *
+ * @idev: [in] Pointer to the internal iommufd_device struct.
+ *
+ */
+void iommufd_unbind_device(struct iommufd_device *idev)
+{
+	struct iommufd_ctx *ictx = idev->ictx;
+
+	xa_erase(&ictx->device_xa, idev->devid);
+	/* Release DMA owner */
+	iommu_device_release_dma_owner(idev->dev, DMA_OWNER_USER);
+	kfree(idev);
+	fput(ictx->filp);
+}
+EXPORT_SYMBOL_GPL(iommufd_unbind_device);
 
 static int __init iommufd_init(void)
 {
