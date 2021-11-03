47a2ec844ec42694872d3c3b1a09f1b870712f78
Nicolin Chen <nicolinc@nvidia.com>
2021-11-02 21:53:53 -0700

    iommu/arm-smmu-v3: Make arm_smmu_cmdq_init reusable
    The CMDQV extension in NVIDIA Tegra241 SoC resues the arm_smmu_cmdq
    structure while the queue location isn't same as smmu->cmdq.
    
    Add a cmdq argument to arm_smmu_cmdq_init() function and shares its
    define in the header for CMDQV driver to use.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -760,6 +760,9 @@ bool arm_smmu_free_asid(struct arm_smmu_ctx_desc *cd);
 int arm_smmu_atc_inv_domain(struct arm_smmu_domain *smmu_domain, int ssid,
 			    unsigned long iova, size_t size);
 
+int arm_smmu_cmdq_init(struct arm_smmu_device *smmu,
+		       struct arm_smmu_cmdq *cmdq);
+
 #ifdef CONFIG_ARM_SMMU_V3_SVA
 bool arm_smmu_sva_supported(struct arm_smmu_device *smmu);
 bool arm_smmu_master_sva_supported(struct arm_smmu_master *master);
