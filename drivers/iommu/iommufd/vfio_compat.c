b02872ed4f3228bae78c45b2df49df5848c5d976
Nicolin Chen <nicolinc@nvidia.com>
2022-01-31 18:39:45 -0800

    iommufd: Add helpers for vfio
    Change-Id: I1fe9338452f34f1ab4087c0d9c9ad8f07e4a7035
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/iommufd/vfio_compat.c
+++ drivers/iommu/iommufd/vfio_compat.c
@@ -336,3 +336,87 @@ int iommufd_vfio_ioctl(struct iommufd_ctx *ictx, unsigned int cmd,
 	}
 	return -ENOIOCTLCMD;
 }
+
+#define vfio_device_detach_unbind(ictx, device, device_list)                   \
+	({                                                                     \
+		list_for_each_entry(device, device_list, group_next) {         \
+			struct vfio_device_detach_ioaspt detach = {            \
+				.argsz = sizeof(detach),                       \
+				.ioaspt_id = ictx->vfio_ioaspt->obj.id,        \
+				.iommufd = ictx->vfio_fd,                      \
+			};                                                     \
+			if (device->ops->detach_ioaspt)                        \
+				device->ops->detach_ioaspt(device, &detach);   \
+			if (device->ops->unbind_iommufd)                       \
+				device->ops->unbind_iommufd(device);           \
+		}                                                              \
+	})
+
+struct iommufd_ctx *vfio_group_set_iommufd(int fd, struct list_head *device_list)
+{
+	struct vfio_device_attach_ioaspt attach = { .argsz = sizeof(attach) };
+	struct vfio_device_bind_iommufd bind = { .argsz = sizeof(bind) };
+	struct iommufd_ctx *ictx = iommufd_fget(fd);
+	struct vfio_device *device;
+	int rc;
+
+	if (!ictx)
+		return ictx;
+
+	ictx->vfio_fd = fd;
+
+	/* FIXME bind.dev_cookie */
+	bind.iommufd = fd;
+	attach.iommufd = fd;
+
+	ictx->vfio_ioaspt = get_compat_ioas(ictx);
+	if (IS_ERR(ictx->vfio_ioaspt))
+		goto out_fput;
+
+	attach.ioaspt_id = ictx->vfio_ioaspt->obj.id;
+
+	list_for_each_entry(device, device_list, group_next) {
+		if (!device->ops->bind_iommufd || !device->ops->unbind_iommufd)
+			goto cleanup_ioaspt;
+
+		rc = device->ops->bind_iommufd(device, &bind);
+		if (rc)
+			goto cleanup_ioaspt;
+
+		if (unlikely(!device->ops->attach_ioaspt)) {
+			goto cleanup_ioaspt;
+		}
+
+		rc = device->ops->attach_ioaspt(device, &attach);
+		if (rc)
+			goto cleanup_ioaspt;
+	}
+
+	return ictx;
+
+cleanup_ioaspt:
+	vfio_device_detach_unbind(ictx, device, device_list);
+	iommufd_ioas_pagetable_destroy(&ictx->vfio_ioaspt->obj);
+out_fput:
+	fput(ictx->filp);
+
+	return NULL;
+}
+EXPORT_SYMBOL_GPL(vfio_group_set_iommufd);
+
+void vfio_group_unset_iommufd(void *iommufd, struct list_head *device_list)
+{
+	struct iommufd_ctx *ictx = (struct iommufd_ctx *)iommufd;
+	struct vfio_device *device;
+
+	if (!ictx)
+		return;
+
+	if (!ictx->vfio_ioaspt)
+		return;
+
+	vfio_device_detach_unbind(ictx, device, device_list);
+	iommufd_ioas_pagetable_destroy(&ictx->vfio_ioaspt->obj);
+	fput(ictx->filp);
+}
+EXPORT_SYMBOL_GPL(vfio_group_unset_iommufd);
