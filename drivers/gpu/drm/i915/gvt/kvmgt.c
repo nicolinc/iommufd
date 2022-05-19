a2e65354404d54a38fc77832b0835ae74419ebc8
Nicolin Chen <nicolinc@nvidia.com>
2022-05-19 01:16:58 -0700

    drm/i915/gvt: Update vfio_pin_pages API
    Change-Id: I25d1236ebfd573817a4bc0c6f3bc1114bbf8ecd6
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/gpu/drm/i915/gvt/kvmgt.c
+++ drivers/gpu/drm/i915/gvt/kvmgt.c
@@ -252,7 +252,7 @@ static void gvt_unpin_guest_page(struct intel_vgpu *vgpu, unsigned long gfn,
 static int gvt_pin_guest_page(struct intel_vgpu *vgpu, unsigned long gfn,
 		unsigned long size, struct page **page)
 {
-	unsigned long base_pfn = 0;
+	struct page *page_base;
 	int total_pages;
 	int npage;
 	int ret;
@@ -264,26 +264,19 @@ static int gvt_pin_guest_page(struct intel_vgpu *vgpu, unsigned long gfn,
 	 */
 	for (npage = 0; npage < total_pages; npage++) {
 		unsigned long cur_gfn = gfn + npage;
-		unsigned long pfn;
+		struct page *page;
 
 		ret = vfio_pin_pages(&vgpu->vfio_device, &cur_gfn, 1,
-				     IOMMU_READ | IOMMU_WRITE, &pfn);
+				     IOMMU_READ | IOMMU_WRITE, &page);
 		if (ret != 1) {
 			gvt_vgpu_err("vfio_pin_pages failed for gfn 0x%lx, ret %d\n",
 				     cur_gfn, ret);
 			goto err;
 		}
 
-		if (!pfn_valid(pfn)) {
-			gvt_vgpu_err("pfn 0x%lx is not mem backed\n", pfn);
-			npage++;
-			ret = -EFAULT;
-			goto err;
-		}
-
 		if (npage == 0)
-			base_pfn = pfn;
-		else if (base_pfn + npage != pfn) {
+			page_base = page;
+		else if (page_base + npage != page) {
 			gvt_vgpu_err("The pages are not continuous\n");
 			ret = -EINVAL;
 			npage++;
@@ -291,7 +284,8 @@ static int gvt_pin_guest_page(struct intel_vgpu *vgpu, unsigned long gfn,
 		}
 	}
 
-	*page = pfn_to_page(base_pfn);
+	*page = page_base;
+
 	return 0;
 err:
 	gvt_unpin_guest_page(vgpu, gfn, npage * PAGE_SIZE);
