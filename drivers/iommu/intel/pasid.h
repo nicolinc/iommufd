hisilicon/kernel-dev

iommu/vt-d: Add support for nested mode translation
It includes:

- Allocate and free nested domains;
- Cache invalidation for nested domains;
- Setup/teardown nested translation structures for transfers w/o pasid.
- Setup/teardown nested translation structures for transfers w/ pasid.

Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>

drivers/iommu/intel/pasid.h
@@ -118,6 +118,11 @@ int intel_pasid_setup_second_level(struct intel_iommu *iommu,
 int intel_pasid_setup_pass_through(struct intel_iommu *iommu,
 				   struct dmar_domain *domain,
 				   struct device *dev, u32 pasid);
+int intel_pasid_setup_nested(struct intel_iommu *iommu,
+			     struct device *dev,
+			     u32 pasid, pgd_t *s1_gpgd,
+			     struct dmar_domain *s2_domain,
+			     struct iommu_stage1_config_vtd *s1_cfg);
 void intel_pasid_tear_down_entry(struct intel_iommu *iommu,
 				 struct device *dev, u32 pasid,
 				 bool fault_ignore);

nicolinc/iommufd

Co-developed-by: Nicolin Chen <nicolinc@nvidia.com>
Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
Co-developed-by: Yi Liu <yi.l.liu@intel.com>
Signed-off-by: Yi Liu <yi.l.liu@intel.com>

drivers/iommu/intel/pasid.h
@@ -118,6 +118,11 @@ int intel_pasid_setup_second_level(struct intel_iommu *iommu,
 int intel_pasid_setup_pass_through(struct intel_iommu *iommu,
 				   struct dmar_domain *domain,
 				   struct device *dev, u32 pasid);
+int intel_pasid_setup_nested(struct intel_iommu *iommu,
+			     struct device *dev,
+			     u32 pasid, pgd_t *s1_gpgd,
+			     struct dmar_domain *s2_domain,
+			     struct iommu_hwpt_intel_vtd *s1_cfg);
 void intel_pasid_tear_down_entry(struct intel_iommu *iommu,
 				 struct device *dev, u32 pasid,
 				 bool fault_ignore);