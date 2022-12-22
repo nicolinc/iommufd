7f072d710b34a40bcebb0f3c26108330a35ba6f7
Nicolin Chen <nicolinc@nvidia.com>
2022-12-21 18:21:32 -0800

    iommu: Introduce a new iommu_group_replace_domain() API
    qemu has a need to replace the translations associated with a domain
    when the guest does large-scale operations like switching between an
    IDENTITY domain and, say, dma-iommu.c.
    
    Currently, it does this by replacing all the mappings in a single
    domain, but this is very inefficient and means that domains have to be
    per-device rather than per-translation.
    
    Provide a high-level API to allow replacements of one domain with
    another. This is similar to a detach/attach cycle except it doesn't
    force the group to go to the blocking domain in-between.
    
    By removing this forced blocking domain the iommu driver has the
    opportunity to implement an atomic replacement of the domains to the
    greatest extent its hardware allows.
    
    Atomic replacement allows the qemu emulation of the viommu to be more
    complete, as real hardware has this ability.
    
    All drivers are already required to support changing between active
    UNMANAGED domains when using their attach_dev ops.
    
    This API is expected to be used by IOMMUFD, so add to the iommu-priv
    header and mark it as IOMMUFD_INTERNAL.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/iommu-priv.h
+++ drivers/iommu/iommu-priv.h
@@ -19,4 +19,16 @@ static inline const struct iommu_ops *dev_iommu_ops(struct device *dev)
 	 */
 	return dev->iommu->iommu_dev->ops;
 }
+
+#ifdef CONFIG_IOMMU_API
+extern int iommu_group_replace_domain(struct iommu_group *group,
+				      struct iommu_domain *new_domain);
+#else
+static inline int iommu_group_replace_domain(struct iommu_group *group,
+					     struct iommu_domain *new_domain)
+{
+	return -ENODEV;
+}
+#endif
+
 #endif /* __LINUX_IOMMU_PRIV_H */
