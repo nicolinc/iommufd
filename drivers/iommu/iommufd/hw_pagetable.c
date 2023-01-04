dc8803d1c45de09989cadd5319d71c229de99133
Yi Liu <yi.l.liu@intel.com>
2023-01-03 23:14:45 -0800

    iommufd/hw_pagetable: Add infrastruture for nested translation
    Under nested translation, device is attached to the stage-1 hw_pagetable,
    while the stage-2 hw_pageable is a background object. This is done by
    storing a parent hw_pagetable in stage-1 hw_pagetable. When doing device
    attachment, two steps as below:
      1) device is firstly "attached" to the stage-2 hw_pagetable. This
         attachment sets up the stage-2 page table e.g. linking the stage-2
         hw_pagetable to IOAS/IOPT if not yet and increasing the device_users.
      2) device is added to the stage-1 hw_pagetable's device_list.
    
    For the stage-1 hw_pagetable in nested, it is maintained by userspace, so
    it requires userspace to provide data for creation. This will be added in
    further commits.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/hw_pagetable.c
+++ drivers/iommu/iommufd/hw_pagetable.c
@@ -16,9 +16,74 @@ void iommufd_hw_pagetable_destroy(struct iommufd_object *obj)
 
 	iommu_domain_free(hwpt->domain);
 	refcount_dec(&hwpt->ioas->obj.users);
-	WARN_ON(!refcount_dec_if_one(hwpt->devices_users));
-	mutex_destroy(hwpt->devices_lock);
-	kfree(hwpt->devices_lock);
+	if (hwpt->parent) {
+		refcount_dec(&hwpt->parent->obj.users);
+	} else {
+		WARN_ON(!refcount_dec_if_one(hwpt->devices_users));
+		mutex_destroy(hwpt->devices_lock);
+		kfree(hwpt->devices_lock);
+	}
+}
+
+static struct iommufd_hw_pagetable *
+__iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx, struct device *dev,
+			     struct iommufd_ioas *ioas, u32 data_type,
+			     struct iommufd_hw_pagetable *parent,
+			     void *user_data, size_t data_len)
+{
+	struct iommu_domain *parent_domain = NULL;
+	struct iommufd_hw_pagetable *hwpt;
+	int rc;
+
+	if (WARN_ON(!ioas && !parent))
+		return ERR_PTR(-EINVAL);
+
+	hwpt = iommufd_object_alloc(ictx, hwpt, IOMMUFD_OBJ_HW_PAGETABLE);
+	if (IS_ERR(hwpt))
+		return hwpt;
+
+	if (parent)
+		parent_domain = parent->domain;
+
+	hwpt->domain = iommu_domain_alloc_user(dev, data_type, parent_domain,
+					       user_data, data_len);
+	if (!hwpt->domain) {
+		rc = -ENOMEM;
+		goto out_abort;
+	}
+
+	INIT_LIST_HEAD(&hwpt->devices);
+	INIT_LIST_HEAD(&hwpt->hwpt_item);
+	hwpt->parent = parent;
+	if (parent) {
+		/* Always reuse parent's devices_lock and devices_users... */
+		hwpt->devices_lock = parent->devices_lock;
+		hwpt->devices_users = parent->devices_users;
+		refcount_inc(&parent->obj.users);
+	} else {
+		/* ...otherwise, allocate a new pair */
+		hwpt->devices_lock = kzalloc(sizeof(*hwpt->devices_lock) +
+					     sizeof(*hwpt->devices_users),
+					     GFP_KERNEL);
+		if (!hwpt->devices_lock) {
+			rc = -ENOMEM;
+			goto out_free_domain;
+		}
+		mutex_init(hwpt->devices_lock);
+		hwpt->devices_users = (refcount_t *)&hwpt->devices_lock[1];
+		refcount_set(hwpt->devices_users, 1);
+	}
+
+	/* Pairs with iommufd_hw_pagetable_destroy() */
+	refcount_inc(&ioas->obj.users);
+	hwpt->ioas = ioas;
+	return hwpt;
+
+out_free_domain:
+	iommu_domain_free(hwpt->domain);
+out_abort:
+	iommufd_object_abort(ictx, &hwpt->obj);
+	return ERR_PTR(rc);
 }
 
 /**
@@ -33,44 +98,7 @@ struct iommufd_hw_pagetable *
 iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx, struct iommufd_ioas *ioas,
 			   struct device *dev)
 {
-	struct iommufd_hw_pagetable *hwpt;
-	int rc;
-
-	if (WARN_ON(!ioas))
-		return ERR_PTR(-EINVAL);
-
-	hwpt = iommufd_object_alloc(ictx, hwpt, IOMMUFD_OBJ_HW_PAGETABLE);
-	if (IS_ERR(hwpt))
-		return hwpt;
-
-	hwpt->domain = iommu_domain_alloc_user(dev, IOMMU_DEVICE_DATA_NONE,
-					       NULL, NULL, 0);
-	if (!hwpt->domain) {
-		rc = -ENOMEM;
-		goto out_abort;
-	}
-
-	INIT_LIST_HEAD(&hwpt->devices);
-	INIT_LIST_HEAD(&hwpt->hwpt_item);
-	hwpt->devices_lock = kzalloc(sizeof(*hwpt->devices_lock) +
-				     sizeof(*hwpt->devices_users),
-				     GFP_KERNEL);
-	if (!hwpt->devices_lock) {
-		rc = -ENOMEM;
-		goto out_free_domain;
-	}
-	mutex_init(hwpt->devices_lock);
-	hwpt->devices_users = (refcount_t *)&hwpt->devices_lock[1];
-	refcount_set(hwpt->devices_users, 1);
-
-	/* Pairs with iommufd_hw_pagetable_destroy() */
-	refcount_inc(&ioas->obj.users);
-	hwpt->ioas = ioas;
-	return hwpt;
-
-out_free_domain:
-	iommu_domain_free(hwpt->domain);
-out_abort:
-	iommufd_object_abort(ictx, &hwpt->obj);
-	return ERR_PTR(rc);
+	return __iommufd_hw_pagetable_alloc(ictx, dev, ioas,
+					    IOMMU_DEVICE_DATA_NONE,
+					    NULL, NULL, 0);
 }
