c423cb5343151833df77f914a4222c7b7f1f44e9
Lu Baolu <baolu.lu@linux.intel.com>
2023-01-11 00:49:13 -0800

    iommu: Add new iommu op to get iommu hardware information
    Introduce a new iommu op get the IOMMU hardware capabilities for iommufd.
    This information will be used by any vIOMMU driver which is owned by
    userspace.
    
    This op chooses to make the special parameters opaque to the core. This
    suits the current usage model where accessing any of the IOMMU device
    special parameters does require a userspace driver that matches the kernel
    driver. If a need for common parameters, implemented similarly by several
    drivers, arises then there's room in the design to grow a generic parameter
    set as well. No warpper API is added as it is supposed to be used by
    iommufd only.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/uapi/linux/iommufd.h
+++ include/uapi/linux/iommufd.h
@@ -370,4 +370,11 @@ struct iommu_hwpt_alloc {
 	__u32 __reserved;
 };
 #define IOMMU_HWPT_ALLOC _IO(IOMMUFD_TYPE, IOMMUFD_CMD_HWPT_ALLOC)
+
+/**
+ * enum iommu_device_data_type - IOMMU hardware Data types
+ */
+enum iommu_device_data_type {
+	IOMMU_DEVICE_DATA_INVALID,
+};
 #endif
