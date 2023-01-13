3c32423b589e5438c75fccf8c71be3b17a9edb88
Yi Liu <yi.l.liu@intel.com>
2023-01-13 03:44:07 -0800

    iommufd: Split iommufd_hw_pagetable_alloc()
    to a helper which accepts the parent hw_pagetable pointer and user_data
    pointer. This is a prepareation for supporting userspace hw_pagetable
    allocation as caller needs to pass the two parameters to domain_alloc_user
    op.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/hw_pagetable.c
+++ drivers/iommu/iommufd/hw_pagetable.c
@@ -27,23 +27,20 @@ void iommufd_hw_pagetable_destroy(struct iommufd_object *obj)
 	mutex_destroy(&hwpt->devices_lock);
 }
 
-/**
- * iommufd_hw_pagetable_alloc() - Get an iommu_domain for a device
- * @ictx: iommufd context
- * @ioas: IOAS to associate the domain with
- * @dev: Device to get an iommu_domain for
- *
- * Allocate a new iommu_domain and return it as a hw_pagetable.
- * iommufd_hw_pagetable_finalize() must be called to successfully complete the
- * allocation, otherwise iommufd_object_abort_and_destroy() should be called.
- */
-struct iommufd_hw_pagetable *
-iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx, struct iommufd_ioas *ioas,
-			   struct device *dev)
+static struct iommufd_hw_pagetable *
+__iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx,
+			     struct iommufd_ioas *ioas,
+			     struct device *dev,
+			     struct iommufd_hw_pagetable *parent,
+			     void *user_data)
 {
 	const struct iommu_ops *ops;
+	struct iommu_domain *parent_domain = NULL;
 	struct iommufd_hw_pagetable *hwpt;
 	int rc;
+
+	if (WARN_ON(!ioas && !parent))
+		return ERR_PTR(-EINVAL);
 
 	hwpt = iommufd_object_alloc(ictx, hwpt, IOMMUFD_OBJ_HW_PAGETABLE);
 	if (IS_ERR(hwpt))
@@ -55,7 +52,10 @@ iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx, struct iommufd_ioas *ioas,
 		goto out_abort;
 	}
 
-	hwpt->domain = ops->domain_alloc_user(dev, NULL, NULL);
+	if (parent)
+		parent_domain = parent->domain;
+
+	hwpt->domain = ops->domain_alloc_user(dev, parent_domain, user_data);
 	if (!hwpt->domain) {
 		rc = -ENOMEM;
 		goto out_abort;
@@ -86,4 +86,21 @@ void iommufd_hw_pagetable_finalize(struct iommufd_ctx *ictx,
 	 */
 	list_add_tail(&hwpt->hwpt_item, &hwpt->ioas->hwpt_list);
 	iommufd_object_finalize(ictx, &hwpt->obj);
+}
+
+/**
+ * iommufd_hw_pagetable_alloc() - Get an iommu_domain for a device
+ * @ictx: iommufd context
+ * @ioas: IOAS to associate the domain with
+ * @dev: Device to get an iommu_domain for
+ *
+ * Allocate a new iommu_domain and return it as a hw_pagetable.
+ * iommufd_hw_pagetable_finalize() must be called to successfully complete the
+ * allocation, otherwise iommufd_object_abort_and_destroy() should be called.
+ */
+struct iommufd_hw_pagetable *
+iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx, struct iommufd_ioas *ioas,
+			   struct device *dev)
+{
+	return __iommufd_hw_pagetable_alloc(ictx, ioas, dev, NULL, NULL);
 }
