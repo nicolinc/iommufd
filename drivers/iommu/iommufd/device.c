26f9a2a4a5e28574295ecfe057e949dcc9a58c7e
Nicolin Chen <nicolinc@nvidia.com>
2024-12-10 00:05:40 +0000

    iommufd: Make attach_handle generic
    "attach_handle" was added exclusively for the iommufd_fault_iopf_handler()
    used by IOPF/PRI use cases, along with the "fault_data". Now, the iommufd
    version of sw_msi function will resue the attach_handle and fault_data for
    a non-fault case.
    
    Move the attach_handle part out of the fault.c file to make it generic for
    all cases.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/iommufd/device.c
+++ drivers/iommu/iommufd/device.c
@@ -352,6 +352,97 @@ iommufd_device_attach_reserved_iova(struct iommufd_device *idev,
 	return 0;
 }
 
+/* The device attach/detach/replace helpers for attach_handle */
+
+static int iommufd_hwpt_attach_device(struct iommufd_hw_pagetable *hwpt,
+				      struct iommufd_device *idev)
+{
+	struct iommufd_attach_handle *handle;
+	int rc;
+
+	if (hwpt->fault) {
+		rc = iommufd_fault_domain_attach_dev(hwpt, idev);
+		if (rc)
+			return rc;
+	}
+
+	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
+	if (!handle) {
+		rc = -ENOMEM;
+		goto out_fault_detach;
+	}
+
+	handle->idev = idev;
+	rc = iommu_attach_group_handle(hwpt->domain, idev->igroup->group,
+				       &handle->handle);
+	if (rc)
+		goto out_free_handle;
+
+	return 0;
+
+out_free_handle:
+	kfree(handle);
+	handle = NULL;
+out_fault_detach:
+	if (hwpt->fault)
+		iommufd_fault_domain_detach_dev(hwpt, idev, handle);
+	return rc;
+}
+
+#define iommufd_device_get_attach_handle(idev)                         \
+	to_iommufd_handle(iommu_attach_handle_get(idev->igroup->group, \
+						  IOMMU_NO_PASID, 0))
+
+static void iommufd_hwpt_detach_device(struct iommufd_hw_pagetable *hwpt,
+				       struct iommufd_device *idev)
+{
+	struct iommufd_attach_handle *handle;
+
+	handle = iommufd_device_get_attach_handle(idev);
+	iommu_detach_group_handle(hwpt->domain, idev->igroup->group);
+	if (hwpt->fault)
+		iommufd_fault_domain_detach_dev(hwpt, idev, handle);
+	kfree(handle);
+}
+
+static int iommufd_hwpt_replace_device(struct iommufd_device *idev,
+				       struct iommufd_hw_pagetable *hwpt,
+				       struct iommufd_hw_pagetable *old)
+{
+	struct iommufd_attach_handle *old_handle =
+		iommufd_device_get_attach_handle(idev);
+	struct iommufd_attach_handle *handle;
+	int rc;
+
+	if (old->fault || hwpt->fault) {
+		rc = iommufd_fault_domain_replace_dev(idev, hwpt, old,
+						      old_handle);
+		if (rc)
+			return rc;
+	}
+
+	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
+	if (!handle) {
+		rc = -ENOMEM;
+		goto out_fault_replace;
+	}
+
+	handle->idev = idev;
+	rc = iommu_replace_group_handle(idev->igroup->group, hwpt->domain,
+					&handle->handle);
+	if (rc)
+		goto out_fault_replace;
+
+	return 0;
+
+out_fault_replace:
+	if (old->fault || hwpt->fault)
+		return iommufd_fault_domain_replace_dev(idev, old, hwpt,
+							handle);
+	kfree(handle);
+	return rc;
+}
+
 int iommufd_hw_pagetable_attach(struct iommufd_hw_pagetable *hwpt,
 				struct iommufd_device *idev)
 {
