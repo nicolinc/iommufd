d78d3bb94d59ef078fb3019962814643249eca7a
Nicolin Chen <nicolinc@nvidia.com>
2024-12-21 07:06:48 +0000

    iommufd: Add iommufd_struct_destroy to revert iommufd_viommu_alloc
    An IOMMU driver that allocated a vIOMMU may want to revert the allocation,
    if it encounters an internal error after the allocation. So, there needs a
    destroy helper for drivers to use.
    
    Move iommufd_object_abort() to the driver.c file and the public header, to
    introduce common iommufd_struct_destroy() helper that will abort all kinds
    of driver structures, not confined to iommufd_viommu but also the new ones
    being added by the following patches.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/linux/iommufd.h
+++ include/linux/iommufd.h
@@ -188,6 +188,7 @@ static inline int iommufd_vfio_compat_set_no_iommu(struct iommufd_ctx *ictx)
 struct iommufd_object *_iommufd_object_alloc(struct iommufd_ctx *ictx,
 					     size_t size,
 					     enum iommufd_object_type type);
+void iommufd_object_abort(struct iommufd_ctx *ictx, struct iommufd_object *obj);
 struct device *iommufd_viommu_find_dev(struct iommufd_viommu *viommu,
 				       unsigned long vdev_id);
 unsigned long iommufd_viommu_get_vdev_id(struct iommufd_viommu *viommu,
@@ -200,6 +201,11 @@ _iommufd_object_alloc(struct iommufd_ctx *ictx, size_t size,
 		      enum iommufd_object_type type)
 {
 	return ERR_PTR(-EOPNOTSUPP);
+}
+
+static inline void iommufd_object_abort(struct iommufd_ctx *ictx,
+					struct iommufd_object *obj)
+{
 }
 
 static inline struct device *
@@ -238,5 +244,14 @@ static inline int iommufd_viommu_report_irq(struct iommufd_viommu *viommu,
 		if (!IS_ERR(ret))                                              \
 			ret->member.ops = viommu_ops;                          \
 		ret;                                                           \
+	})
+
+/* Helper for IOMMU driver to destroy structures created by allocators above */
+#define iommufd_struct_destroy(ictx, drv_struct, member)                       \
+	({                                                                     \
+		static_assert(__same_type(struct iommufd_object,               \
+					  drv_struct->member.obj));            \
+		static_assert(offsetof(typeof(*drv_struct), member.obj) == 0); \
+		iommufd_object_abort(ictx, &drv_struct->member.obj);           \
 	})
 #endif
