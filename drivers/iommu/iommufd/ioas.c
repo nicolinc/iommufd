525b9ffa94dc5842f98bef8cbcbf426a2a8292be
Nicolin Chen <nicolinc@nvidia.com>
2025-02-07 01:13:34 +0000

    iommufd: Add IOMMU_OPTION_SW_MSI_START/SIZE ioctls
    For systems that require MSI pages to be mapped into the IOMMU translation
    the IOMMU driver provides an IOMMU_RESV_SW_MSI range, which is the default
    recommended IOVA window to place these mappings. However, there is nothing
    special about this address. And to support the RMR trick in VMM for nested
    translation, the VMM needs to know what sw_msi window the kernel is using.
    
    As there is no particular reason to force VMM to adopt the kernel default,
    provide a simple IOMMU_OPTION_SW_MSI_START/SIZE ioctl that the VMM can use
    to directly specify its desired sw_msi window, which replaces and disables
    the default IOMMU_RESV_SW_MSI from the driver, to avoid having to build an
    API to discover the default IOMMU_RESV_SW_MSI.
    
    Since iommufd now has its own sw_msi function, this is easy to implement.
    
    Keep these two options per iommufd_device, so each device can set its own
    desired MSI window. VMM must set the values before attaching the device to
    any HWPT/IOAS to have an effect.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/iommufd/ioas.c
+++ drivers/iommu/iommufd/ioas.c
@@ -620,6 +620,103 @@ int iommufd_option_rlimit_mode(struct iommu_option *cmd,
 	return -EOPNOTSUPP;
 }
 
+static inline int iommufd_option_sw_msi_test(struct iommufd_device *idev,
+					     phys_addr_t start, size_t size)
+{
+	const phys_addr_t alignment = SZ_1M - 1;
+	struct iommu_resv_region *resv;
+	phys_addr_t resv_last, last;
+	LIST_HEAD(resv_regions);
+	int rc = 0;
+
+	/* Alignment Test */
+	if (start & alignment)
+		return -EINVAL;
+
+	/* Overlap Test */
+	if (!size)
+		size = SZ_1M;
+	last = size - 1 + start;
+	/* FIXME: drivers allocate memory but there is no failure propogated */
+	iommu_get_resv_regions(idev->dev, &resv_regions);
+	list_for_each_entry(resv, &resv_regions, list) {
+		if (resv->type == IOMMU_RESV_SW_MSI || /* iommufd will bypass */
+		    resv->type == IOMMU_RESV_DIRECT_RELAXABLE)
+			continue;
+		resv_last = resv->length - 1 + resv->start;
+		if (resv->start <= last && resv_last >= start) {
+			rc = -EADDRINUSE;
+			break;
+		}
+	}
+	iommu_put_resv_regions(idev->dev, &resv_regions);
+	return rc;
+}
+
+int iommufd_option_sw_msi(struct iommufd_ucmd *ucmd)
+{
+	struct iommu_option *cmd = ucmd->cmd;
+	struct iommufd_device *idev;
+	int rc = 0;
+
+	idev = iommufd_get_device(ucmd, cmd->object_id);
+	if (IS_ERR(idev))
+		return PTR_ERR(idev);
+
+	mutex_lock(&idev->igroup->lock);
+	/* Device cannot enforce the sw_msi window if already attached */
+	if (idev->igroup->hwpt) {
+		rc = -EBUSY;
+		goto out_unlock;
+	}
+
+	if (cmd->op == IOMMU_OPTION_OP_GET) {
+		switch (cmd->option_id) {
+		case IOMMU_OPTION_SW_MSI_START:
+			cmd->val64 = (u64)idev->sw_msi_start;
+			break;
+		case IOMMU_OPTION_SW_MSI_SIZE:
+			cmd->val64 = (u64)idev->sw_msi_size / SZ_1M;
+			break;
+		default:
+			rc = -EOPNOTSUPP;
+			break;
+		}
+	}
+	if (cmd->op == IOMMU_OPTION_OP_SET) {
+		phys_addr_t start = idev->sw_msi_start;
+		size_t size = idev->sw_msi_size;
+
+		switch (cmd->option_id) {
+		case IOMMU_OPTION_SW_MSI_START:
+			start = (phys_addr_t)cmd->val64;
+			rc = iommufd_option_sw_msi_test(idev, start, size);
+			if (rc)
+				break;
+			idev->sw_msi_start = start;
+			break;
+		case IOMMU_OPTION_SW_MSI_SIZE:
+			size = (size_t)cmd->val64 * SZ_1M;
+			if (size) {
+				rc = iommufd_option_sw_msi_test(idev, start,
+								size);
+				if (rc)
+					break;
+			}
+			idev->sw_msi_size = size;
+			break;
+		default:
+			rc = -EOPNOTSUPP;
+			break;
+		}
+	}
+
+out_unlock:
+	mutex_unlock(&idev->igroup->lock);
+	iommufd_put_object(ucmd->ictx, &idev->obj);
+	return rc;
+}
+
 static int iommufd_ioas_option_huge_pages(struct iommu_option *cmd,
 					  struct iommufd_ioas *ioas)
 {
