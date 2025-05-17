d75b14f77997836603650bb12437ccaf50afd2a6
Nicolin Chen <nicolinc@nvidia.com>
2025-05-17 06:46:20 +0000

    iommu: Allow an input type in hw_info op
    The hw_info uAPI will support a bidirectional data_type field that can be
    used as an input field for user space to request for a specific info data.
    
    To prepare for the uAPI update, change the iommu layer first:
     - Add a new IOMMU_HW_INFO_TYPE_DEFAULT as an input, for which driver can
       output its only (or firstly) supported type
     - Update the kdoc accordingly
     - Roll out the type validation in the existing drivers
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -607,8 +607,10 @@ __iommu_copy_struct_to_user(const struct iommu_user_data *dst_data,
  * @capable: check capability
  * @hw_info: report iommu hardware information. The data buffer returned by this
  *           op is allocated in the iommu driver and freed by the caller after
- *           use. The information type is one of enum iommu_hw_info_type defined
- *           in include/uapi/linux/iommufd.h.
+ *           use. @type can input a requested type and output a supported type,
+ *           either of which must be defined in enum iommu_hw_info_type in
+ *           include/uapi/linux/iommufd.h. Driver should reject an unsupported
+ *           data @type input
  * @domain_alloc: allocate and return an iommu domain if success. Otherwise
  *                NULL is returned. The domain is not fully initialized until
  *                the caller iommu_domain_alloc() returns.
