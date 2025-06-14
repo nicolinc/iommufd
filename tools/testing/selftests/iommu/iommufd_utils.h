37ddc1e9db56ea773cf8882d258327d9c49d39ce
Nicolin Chen <nicolinc@nvidia.com>
2025-06-14 09:07:33 +0000

    iommufd/selftest: Add missing close(mfd) in memfd_mmap()
    Do not forget to close mfd in the error paths, since none of the callers
    would close it when ASSERT_NE(MAP_FAILED, buf) fails.
    
    Fixes: 0bcceb1f51c7 ("iommufd: Selftest coverage for IOMMU_IOAS_MAP_FILE")
    Cc: stable@vger.kernel.org
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- tools/testing/selftests/iommu/iommufd_utils.h
+++ tools/testing/selftests/iommu/iommufd_utils.h
@@ -60,13 +60,19 @@ static inline void *memfd_mmap(size_t length, int prot, int flags, int *mfd_p)
 {
 	int mfd_flags = (flags & MAP_HUGETLB) ? MFD_HUGETLB : 0;
 	int mfd = memfd_create("buffer", mfd_flags);
+	void *buf;
 
 	if (mfd <= 0)
 		return MAP_FAILED;
-	if (ftruncate(mfd, length))
+	if (ftruncate(mfd, length)) {
+		close(mfd);
 		return MAP_FAILED;
+	}
 	*mfd_p = mfd;
-	return mmap(0, length, prot, flags, mfd, 0);
+	buf = mmap(0, length, prot, flags, mfd, 0);
+	if (buf == MAP_FAILED)
+		close(mfd);
+	return buf;
 }
 
 /*
