3cdf066a12138d821aca15e1f740403b9cc2afe0
Nicolin Chen <nicolinc@nvidia.com>
2025-06-28 00:33:39 +0000

    iommu: Lock group->mutex in iommu_deferred_attach
    The iommu_deferred_attach() is a runtime asynchronous function called by
    iommu-dma function, which could race against other attach functions if it
    accesses something in the dev->iommu_group.
    
    So, grab the mutex to guard __iommu_attach_device() like other callers.
    
    For non attach_deffered cases, add a fast path avoiding the mutex. This,
    however, has to turn the "attach_deffered" into a bool type, as READ_ONCE
    cannot take the address of a bit-field.
    
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -2151,10 +2151,17 @@ EXPORT_SYMBOL_GPL(iommu_attach_device);
 
 int iommu_deferred_attach(struct device *dev, struct iommu_domain *domain)
 {
-	if (dev->iommu && dev->iommu->attach_deferred)
-		return __iommu_attach_device(domain, dev);
+	/*
+	 * This is called on the dma mapping fast path so avoid locking. This is
+	 * racy, but we have an expectation that the driver will setup its DMAs
+	 * inside probe while still single threaded to avoid racing.
+	 */
+	if (!dev->iommu || !dev->iommu->attach_deferred)
+		return 0;
 
-	return 0;
+	guard(mutex)(&dev->iommu_group->mutex);
+
+	return __iommu_attach_device(domain, dev);
 }
 
 void iommu_detach_device(struct iommu_domain *domain, struct device *dev)
